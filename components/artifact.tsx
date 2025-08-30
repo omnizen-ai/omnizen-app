92365988363df9091773b7aa8a782e7d63439499
kalam360 <abulkalamfaruk@gmail.com>
2025-08-30 19:49:50 +0600

    feat: adjust width of artifact display components for better responsiveness

--- components/artifact.tsx
+++ components/artifact.tsx
@@ -279,7 +279,7 @@ function PureArtifact({
 
           {!isMobile && (
             <motion.div
-              className="relative w-[400px] bg-muted dark:bg-background h-dvh shrink-0"
+              className="relative w-1/2 bg-muted dark:bg-background h-dvh shrink-0"
               initial={{ opacity: 0, x: 10, scale: 1 }}
               animate={{
                 opacity: 1,
@@ -302,7 +302,7 @@ function PureArtifact({
               <AnimatePresence>
                 {!isCurrentVersion && (
                   <motion.div
-                    className="left-0 absolute h-dvh w-[400px] top-0 bg-zinc-900/50 z-50"
+                    className="left-0 absolute h-dvh w-1/2 top-0 bg-zinc-900/50 z-50"
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
@@ -382,12 +382,10 @@ function PureArtifact({
                   }
                 : {
                     opacity: 1,
-                    x: 400,
+                    x: windowWidth ? windowWidth / 2 : '50%',
                     y: 0,
                     height: windowHeight,
-                    width: windowWidth
-                      ? windowWidth - 400
-                      : 'calc(100dvw-400px)',
+                    width: windowWidth ? windowWidth / 2 : '50%',
                     borderRadius: 0,
                     transition: {
                       delay: 0,
